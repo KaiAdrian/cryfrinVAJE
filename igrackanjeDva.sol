027d55d56bb470d1562ea52d39c4f2c75f03bb17
KaiAdrian <thriller30@gmail.com>
2025-03-26 12:54:11 +0100

    struct

--- igrackanjeDva.sol
+++ igrackanjeDva.sol
@@ -3,14 +3,21 @@ pragma solidity ^0.8.18;
 
 contract Pagadba {
 
-mapping (uint256 => string) public pliskoti;
+// mapping (uint256 => string) public pliskoti;
    
-   constructor (){
-        pliskoti[0] = "Opica";
-        pliskoti[1] = "Medo";
-        pliskoti[2] = "Rino";
-        pliskoti[3] = "Zajcek";
-        pliskoti[4] = "Haskey";
-        pliskoti[5] = "Kabilca";    
-   }
+//    constructor (){
+//         pliskoti[0] = "Opica";
+//         pliskoti[1] = "Medo";
+//         pliskoti[2] = "Rino";
+//         pliskoti[3] = "Zajcek";
+//         pliskoti[4] = "Haskey";
+//         pliskoti[5] = "Kabilca";    
+//    }
+
+struct Plisko{
+   string ime;
+   uint256 starost;
+   bool  vZvezi;
+}
+
 }
