138ece01b4f43b7fe4ebcdbd6e73d131d4e5bf33
KaiAdrian <thriller30@gmail.com>
2025-04-03 14:52:01 +0200

    //payable + viewDonator()

--- donacije.sol
+++ donacije.sol
@@ -8,19 +8,23 @@ contract Fundacija {
         address donatorAdress;
         uint256 donacija;
     }
-    // Donator[] public  donatorsList;
-      mapping(address => uint256) public donatorsList;
+    Donator[] public  donatorsList;
+    //   mapping(address => uint256) public donatorsList;
 
-    // function addDonator(uint256 _donacija) public {
-    //     donatorsList.push(Donator(msg.sender, _donacija));
+    function addDonator(uint256 _donacija) public   {
+        donatorsList.push(Donator(msg.sender, _donacija));
+    }
+
+    function viewDonator() public view returns (Donator[] memory) {
+       return donatorsList;
+    }
+    //   function addDonacija(uint256 _amount) public {
+    //     donatorsList[msg.sender] += _amount;
     // }
-      function addDonacija(uint256 _amount) public {
-        donatorsList[msg.sender] += _amount;
-    }
     ///getter
-    function getDonacija(address _sender) public  view returns (uint256){
-        return donatorsList[_sender];  
-    }
+    // function getDonacija() public  view returns (uint256){
+    //     return donatorsList[msg.sender];  
+    // }
 //     // function getDonationByAddress(address _addr) public view returns (uint256) {
 //     // for (uint256 i = 0; i < donatorsList.length; i++) {
 //     //     if (donatorsList[i].donatorAddress == _addr) {
@@ -29,6 +33,8 @@ contract Fundacija {
 //     // }
 //     // return 0; // Če naslov ni najden, vrne 0
 // }
-    
+    function addPrispevek() external payable {
+        require(msg.value>10000,'Prispevek je ni\u017Eji od minimalnega');
+    }
 }
 
